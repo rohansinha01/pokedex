7bc1c24b8618d8e6b6b8dba21c3931daa1de7071
rohansinha01 <rohansinha01@gmail.com>
2023-12-16 18:13:11 -0500

    add title to page

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 //Import Express
-const express = require("express")
+const express = require("express");
 //Import Morgan
-const morgan = require("morgan")
+const morgan = require("morgan");
 //Import Method Override
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
+
+
+
+// //Import the Pokemon Router
+// const pokeRouter = require("./controller/pokemon.js");
+//Import our Pokemon
+const pokemon = require("./models/pokemon.js");
+
 //Create App Object
-const app = express()
+const app = express();
 
 //Middleware
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
-app.use(morgan("dev"))
-app.use(methodOverride("_method"))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
+
+//Routes
+app.get("/", (req,res) => {
+    res.render("pokemon/index.ejs", { data: pokemon})
+})
+// //Register the Pokedex Router
+// app.use("/pokemon", pokeRouter);
 
 //Server Listener
 app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+  console.log("listening on port 3000");
+});
